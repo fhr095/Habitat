aa61eb707284f9306af866a9b211aa506e0f9760
Yuriferr <yurifernandespreto@gmail.com>
2024-05-09 11:41:44 -0300

    vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Set the base path, it's important for deployment on subdirectories
+  build: {
+    outDir: 'dist', // Specify the output directory (default is 'dist')
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'), // Your main HTML entry point
+      },
+      // Additional configuration here if necessary
+    }
+  },
+  resolve: {
+    alias: {
+      // Resolve paths to avoid relative path confusion, optional
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000, // Default development port; change as necessary
+  }
+});
