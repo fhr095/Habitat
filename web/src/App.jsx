4a981ce8716fee17b13e5d244719be85c5f708f5
Yuriferr <yurifernandespreto@gmail.com>
2024-05-14 10:23:28 -0300

    teste

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import SceneScreen from './screens/SceneScreen';
-import ChatScreen from './screens/ChatScreen'; // Assume you have this component
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import SceneScreen from "./screens/SceneScreen";
+import ChatScreen from "./screens/ChatScreen"; // Import the ChatScreen component
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <div className='app-container'>
-        <Routes>
-          <Route path="/" element={<SceneScreen />} />
-          <Route path="/chat" element={<ChatScreen />} />
-        </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/chat">Chat</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <SceneScreen />
+          </Route>
+          <Route path="/chat">
+            <ChatScreen />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
 }
+
+export default App;
