eec6e722ea1fef4fb17d7f34467d157126167570
Yuriferr <yurifernandespreto@gmail.com>
2024-07-02 09:43:51 -0300

    widgets aparecendo

--- web/src/screens/Scene/components/Widget.jsx
+++ web/src/screens/Scene/components/Widget.jsx
@@ -31,51 +31,57 @@ export default function Widget({ habitatId }) {
     setActiveIndex(selectedIndex);
   };
 
-  return (
-    <div className={`widget-container ${expanded ? "expanded" : "collapsed"}`}>
-      {expanded ? (
-        <div className="carousel-wrapper">
-          <button className="toggle-button" onClick={handleToggle}>
-            <FaEyeSlash />
-          </button>
-          <Carousel
-            activeIndex={activeIndex}
-            onSelect={handleSelect}
-            controls={false}
-            indicators={false}
-            interval={3000}
-          >
-            {widgets.map((widget, index) => (
-              <Carousel.Item key={index}>
-                <div className="carousel-content">
-                  <img
-                    className="d-block w-100 carousel-image"
-                    src={widget.imageUrl}
-                    alt={`Widget ${index + 1}`}
-                  />
-                  <div className="carousel-text">
-                    <p>{widget.text}</p>
+  if(widgets.length !== 0) {
+    return (
+      <div className={`widget-container ${expanded ? "expanded" : "collapsed"}`}>
+        {expanded ? (
+          <div className="carousel-wrapper">
+            <button className="toggle-button" onClick={handleToggle}>
+              <FaEyeSlash />
+            </button>
+            <Carousel
+              activeIndex={activeIndex}
+              onSelect={handleSelect}
+              controls={false}
+              indicators={false}
+              interval={3000}
+            >
+              {widgets.map((widget, index) => (
+                <Carousel.Item key={index}>
+                  <div className="carousel-content">
+                    <img
+                      className="d-block w-100 carousel-image"
+                      src={widget.imageUrl}
+                      alt={`Widget ${index + 1}`}
+                    />
+                    <div className="carousel-text">
+                      <p>{widget.text}</p>
+                    </div>
                   </div>
-                </div>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-          <div className="carousel-dots">
-            {widgets.map((_, index) => (
-              <div
-                key={index}
-                className={`dot ${index === activeIndex ? "active" : ""}`}
-                onClick={() => handleSelect(index)}
-              ></div>
-            ))}
+                </Carousel.Item>
+              ))}
+            </Carousel>
+            <div className="carousel-dots">
+              {widgets.map((_, index) => (
+                <div
+                  key={index}
+                  className={`dot ${index === activeIndex ? "active" : ""}`}
+                  onClick={() => handleSelect(index)}
+                ></div>
+              ))}
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="toggle-bar" onClick={handleToggle}>
-          Ver mais
-          <FaEye size={20} />
-        </div>
-      )}
-    </div>
-  );
+        ) : (
+          <div className="toggle-bar" onClick={handleToggle}>
+            Ver mais
+            <FaEye size={20} />
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <></>
+    )
+  } 
 }
