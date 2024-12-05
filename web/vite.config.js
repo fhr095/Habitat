bb1e950ce61b3cc4247488b330fc58871a7612f5
fhr095 <143469663+fhr095@users.noreply.github.com>
2024-12-05 17:35:31 -0300

    Inicializa projeto e adiciona documentação MWM

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { VitePWA } from 'vite-plugin-pwa';
+
+
 
 export default defineConfig({
   server: {
@@ -8,6 +11,36 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Seu Nome de Aplicação',
+        short_name: 'Nome Curto',
+        description: 'Descrição da sua aplicação',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 25 * 1024 * 1024, // Limite para 25 MB
+      },
+    }),
     viteStaticCopy({
       targets: [
         {
@@ -26,13 +59,13 @@ export default defineConfig({
     })
   ],
   build: {
-    chunkSizeWarningLimit: 1000,  // Aumenta o limite de chunk para evitar warnings
+    chunkSizeWarningLimit: 1500,  // Aumenta o limite de chunk para evitar warnings
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom']  // Separa pacotes 'vendor' como react e react-dom
         }
       }
-    }
+    },
   }
 });
